a7fe8433c680a88d8e03436c9770e3e4141aa330
Aniket Roy <royaniket2212@gmail.com>
2026-02-23 05:08:32 +0530

    Add supporting files

---
+++ src/components/SkillMarquee.jsx
@@ -0,0 +1,40 @@
+import { motion } from "motion/react";
+
+import "../styles/marqueeTest.css";
+
+const imgArray = [
+	"./assets/java-logo.png",
+	"./assets/c-logo.png",
+	"./assets/js-logo.png",
+	"./assets/postgresql-logo.png",
+	"./assets/bootstrap-logo.png",
+	"./assets/node-logo.png",
+	"./assets/rest-logo.png",
+	"./assets/react-logo.png"
+];
+
+function SkillMarquee()
+{
+	return (
+		<div className="marquee-container">
+			<motion.div
+				className="group"
+				animate={{x: [0, "-50%"]}}
+				transition={{
+					repeat: Infinity,
+					repeatType: "loop",
+					duration: 10,
+					ease: "linear",
+				}}
+			>
+				{[...imgArray, ...imgArray].map((src, i) => (
+					<div key={i} className="slide">
+						<img src={src} alt={`Slide${i}`} />
+					</div>
+				))}
+			</motion.div>
+		</div>
+	);
+}
+
+export default SkillMarquee;
