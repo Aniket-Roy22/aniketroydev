b359b0b87f35ddd44e83ef92e6bb2e4a20c47dbe
Aniket Roy <royaniket2212@gmail.com>
2026-03-12 02:52:26 +0530

    Add spotify player of current show.

--- src/components/BentoBox.jsx
+++ src/components/BentoBox.jsx
@@ -6,37 +6,32 @@ function BentoBox() {
 	return (
 		<div className="container">
 			<div style={{gridArea: "box1"}}>
-				Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-				Laboriosam voluptatum officia fuga reiciendis rerum voluptatem
-				aspernatur nesciunt adipisci hic, eaque accusamus illo
-				perferendis voluptates quod nobis fugiat quos nulla dolore
-				obcaecati? Blanditiis dolores consequuntur architecto harum sit
-				aspernatur aut eius doloribus, delectus cupiditate. A ipsam sed
-				blanditiis molestiae, magnam provident.
-			</div>
-
-			<div style={{gridArea: "box2"}}>
-				Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-				Perferendis optio, voluptates repudiandae minus quaerat
-				temporibus recusandae repellendus, eos ipsam accusamus sint
-				dolores pariatur possimus ducimus neque tempore qui facere natus
-				debitis iste, consequatur corporis! Rerum minus minima
-				voluptatem corporis recusandae, odit suscipit ipsam fugiat quo
-				quasi iusto, maiores facilis soluta.
-			</div>
-
-			<div style={{gridArea: "box3"}}>
 				<SkillMarquee />
 			</div>
 
-			<div style={{gridArea: "box4"}}>
-				Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-				Perferendis optio, voluptates repudiandae minus quaerat
-				temporibus recusandae repellendus, eos ipsam accusamus sint
-				dolores pariatur possimus ducimus neque tempore qui facere natus
-				debitis iste, consequatur corporis! Rerum minus minima
-				voluptatem corporis recusandae, odit suscipit ipsam fugiat quo
-				quasi iusto, maiores facilis soluta.
+			<div style={{gridArea: "box2"}}>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet
+				cupiditate placeat commodi voluptas quisquam dicta facere,
+				officiis ullam iure voluptate rerum quidem distinctio nisi
+				porro. Dolor tenetur illo dolorum dignissimos.
+			</div>
+
+			<div style={{gridArea: "box3"}}>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam
+				quae aspernatur ad, inventore reprehenderit ex quis earum maxime
+				amet omnis pariatur porro fuga facere modi atque! Cupiditate
+				vero quasi ipsum.
+			</div>
+
+			<div className="music" style={{gridArea: "box4"}}>
+				<span className="music-heading">Currently Listening to...</span>
+				<iframe
+					className="spotify-widget"
+					data-testid="embed-iframe"
+					src="https://open.spotify.com/embed/show/1Y9gTi9bBYEJya1Z8Wo2ME?utm_source=generator"
+					allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+					loading="lazy"
+				></iframe>
 			</div>
 
 			<div style={{gridArea: "box5"}}>
