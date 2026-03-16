b5cc337923f5e433d760d19edc3650a3c311057c
Aniket Roy <royaniket2212@gmail.com>
2026-03-17 04:14:28 +0530

    Fix footer layout, and add final layout fixes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,9 @@ import "../styles/footer.css";
 function Footer()
 {
 	return (
-		<p className="footer">&lt;3 Made with love. &lt;/3</p>
+		<div className="footer">
+			<p>&lt;3 Made with love. &lt;/3</p>
+		</div>
 	);
 };
 
